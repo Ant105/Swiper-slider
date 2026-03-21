ac59d865a8a8ab762f85838007e2f9022921ca61
Maria T <Ant105@users.noreply.github.com>
2026-03-21 16:52:10 +0200

    c

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Thumbnails slider settings */
 const thumbsSwiper = new Swiper(".capybara-slider__thumbs", {
   slidesPerView: "auto",
   spaceBetween: 5,
@@ -9,31 +10,22 @@ const thumbsSwiper = new Swiper(".capybara-slider__thumbs", {
   },
 });
 
+/* Main slider (large images) settings */
 const mainSwiper = new Swiper(".capybara-slider__main", {
   grabCursor: true,
   speed: 450,
-  threshold: 2,
   longSwipesRatio: 0.15,
-  longSwipesMs: 300,
   keyboard: {
     enabled: true,
   },
+  // Connect main slider with thumbnails slider
   thumbs: {
     swiper: thumbsSwiper,
   },
-  pagination: {
-    el: ".capybara-slider__pagination",
-    clickable: true,
-  },
 });
 
+/* Disable image dragging for browsers that don't support -webkit-user-drag */
 const slidersImages = document.querySelectorAll(".capybara-slider__image");
 slidersImages.forEach(function (img) {
   img.setAttribute("draggable", "false");
-});
-
-// пригождалось для теста работы srcset
-const images = document.querySelectorAll(".capybara-slider__image");
-images.forEach(function (img) {
-  console.log(img.currentSrc);
 });
